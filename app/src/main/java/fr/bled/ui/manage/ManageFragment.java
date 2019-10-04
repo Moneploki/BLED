984a0bc60b7f5971fd3eb279c618ecdf06afa5c6
Thomas DAVID <thomas.david@reseau.eseo.fr>
2019-10-04 22:48:16 +0100

    implement color picker

--- app/src/main/java/fr/bled/ui/manage/ManageFragment.java
+++ app/src/main/java/fr/bled/ui/manage/ManageFragment.java
@@ -1,23 +1,40 @@
 package fr.bled.ui.manage;
 
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 import fr.bled.R;
 
 public class ManageFragment extends Fragment {
 
-    private ManageViewModel manageViewModel;
+    public ManageViewModel manageViewModel;
+    public LinearLayout mlayout;
+    public int defaultColor;
+    public Button mButton;
+    public ColorPickerDialogBuilder colorPicker;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         manageViewModel =
@@ -30,6 +47,45 @@ public class ManageFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        mlayout = root.findViewById(R.id.layoutManage);
+        defaultColor = getContext().getColor(R.color.colorPrimary);
+        mButton = (Button) root.findViewById(R.id.buttonPickColor);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            openColorPicker();
+            }
+        });
         return root;
+    }
+
+    public void openColorPicker() {
+        colorPicker = ColorPickerDialogBuilder.with(getContext());
+        colorPicker
+                .setTitle("Choose color")
+                .initialColor(defaultColor)
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        defaultColor=selectedColor;
+                        mlayout.setBackgroundColor(selectedColor);
+                        Toast.makeText(getContext(),"onColorSelected: 0x" + Integer.toHexString(selectedColor),Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
     }
 }
