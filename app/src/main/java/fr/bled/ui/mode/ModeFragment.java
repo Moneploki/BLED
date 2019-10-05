1f5c0a118e5d563324936b8ddf8d3502595b6e87
skyultime <32228139+skyultime@users.noreply.github.com>
2019-10-05 04:31:38 +0200

    BLED -- Mode section initialized (Bug unsolved l.29 in ModeFragment.java
    Signed-off-by: skyultime <32228139+skyultime@users.noreply.github.com>

--- app/src/main/java/fr/bled/ui/mode/ModeFragment.java
+++ app/src/main/java/fr/bled/ui/mode/ModeFragment.java
@@ -3,8 +3,13 @@ package fr.bled.ui.mode;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.Toast;
+import android.graphics.PorterDuff;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -14,10 +19,14 @@ import androidx.lifecycle.ViewModelProviders;
 
 import fr.bled.R;
 
+
 public class ModeFragment extends Fragment {
 
-    private ModeViewModel modeViewModel;
+    int button_nb = 9-1;
+    int i ;
 
+    private ModeViewModel modeViewModel;
+    private java.lang.String button_name_t [] ={"Blink","Blinkalt","Sparkle","Sparkle 2","Strobo","Cycle colors","Pixel shift left","Pixel shift right","Pixel bounce"};
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         modeViewModel =
@@ -30,6 +39,47 @@ public class ModeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        ImageButton simpleImageButton_0 = root.findViewById(R.id.imageButton1);
+        ImageButton simpleImageButton_1 = root.findViewById(R.id.imageButton2);
+        ImageButton simpleImageButton_2 = root.findViewById(R.id.imageButton3);
+        ImageButton simpleImageButton_3 = root.findViewById(R.id.imageButton4);
+        ImageButton simpleImageButton_4 = root.findViewById(R.id.imageButton5);
+        ImageButton simpleImageButton_5 = root.findViewById(R.id.imageButton6);
+        ImageButton simpleImageButton_6 = root.findViewById(R.id.imageButton7);
+        ImageButton simpleImageButton_7 = root.findViewById(R.id.imageButton8);
+        ImageButton simpleImageButton_8 = root.findViewById(R.id.imageButton9);
+
+        ImageButton button_list [] = {simpleImageButton_0, simpleImageButton_1, simpleImageButton_2,simpleImageButton_3, simpleImageButton_4, simpleImageButton_5,simpleImageButton_6, simpleImageButton_7, simpleImageButton_8};
+
+        for (i=0;i<button_nb;i++) {
+            // perform click event on button's
+
+            button_list[i].setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN: {
+
+                            v.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                            v.invalidate();
+                            break;
+                        }
+                        case MotionEvent.ACTION_UP:
+                            Toast.makeText(getContext(), button_name_t[i], Toast.LENGTH_LONG).show();
+                            // Registrer value
+                        case MotionEvent.ACTION_CANCEL: {
+
+                            v.getBackground().clearColorFilter();
+                            v.invalidate();
+                            break;
+                        }
+                    }
+                    return true;
+                }
+            });
+        }
+
         return root;
     }
 }
