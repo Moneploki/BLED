ca5b03901b7c0ae8d0e901951ed5b22e0b59cf73
skyultime <32228139+skyultime@users.noreply.github.com>
2019-10-08 22:17:01 +0200

    BLED -- Switch bluetooth / Wifi : Problème pour changer la couleur des icônes !
    Signed-off-by: skyultime <32228139+skyultime@users.noreply.github.com>

--- app/src/main/java/fr/bled/MainActivity.java
+++ app/src/main/java/fr/bled/MainActivity.java
@@ -1,5 +1,7 @@
 package fr.bled;
 
+import android.app.AlertDialog;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,7 +14,6 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.graphics.Color;
-
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -22,59 +23,65 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+import android.util.Log;
+import java.util.logging.Logger;
 
-    private AppBarConfiguration mAppBarConfiguration;
-    Boolean led_state = false;
-    String led_status = "Off";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        final FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                led_state = ! led_state;
-                led_status = (led_state == true) ?  "On" : "Off";
-                //button.setColorFilter(Color.argb(255, 255, 255, 0));
+                public class MainActivity extends AppCompatActivity {
 
-                if (led_state) {
-                    fab.setColorFilter(Color.argb(255, 255, 255, 0));
-                }else {
-                    fab.clearColorFilter();
+                    private AppBarConfiguration mAppBarConfiguration;
+                    Boolean led_state = false;
+                    String led_status = "Off";
+
+                    @Override
+                    protected void onCreate(Bundle savedInstanceState) {
+                        super.onCreate(savedInstanceState);
+
+
+                        setContentView(R.layout.activity_main);
+                        Toolbar toolbar = findViewById(R.id.toolbar);
+                        setSupportActionBar(toolbar);
+                        final FloatingActionButton fab = findViewById(R.id.fab);
+                        fab.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                            public void onClick(View view) {
+                                led_state = !led_state;
+                                led_status = (led_state == true) ? "On" : "Off";
+
+                                if (led_state) {
+                                    fab.setColorFilter(Color.argb(255, 255, 255, 0));
+                                } else {
+                                    fab.clearColorFilter();
+                                }
+                                Snackbar.make(view, "Light " + led_status, Snackbar.LENGTH_SHORT)
+                                        .setAction("Action", null).show();
+                            }
+                        });
+                        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                        NavigationView navigationView = findViewById(R.id.nav_view);
+                        // Passing each menu ID as a set of Ids because each
+                        // menu should be considered as top level destinations.
+                        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                                R.id.nav_home, R.id.nav_mode, R.id.nav_manage, R.id.nav_music,
+                                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                                .setDrawerLayout(drawer)
+                                .build();
+                        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+                        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+                        NavigationUI.setupWithNavController(navigationView, navController);
+                    }
+
+                    @Override
+                    public boolean onCreateOptionsMenu(Menu menu) {
+                        // Inflate the menu; this adds items to the action bar if it is present.
+                        getMenuInflater().inflate(R.menu.main, menu);
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onSupportNavigateUp() {
+                        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+                        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                                || super.onSupportNavigateUp();
+                    }
                 }
-                    Snackbar.make(view, "Light "+led_status, Snackbar.LENGTH_SHORT)
-                            .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_mode, R.id.nav_manage,R.id.nav_music,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-}
