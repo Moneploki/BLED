b6791932543b569f888d6e0316e290384cb70e1d
skyultime <32228139+skyultime@users.noreply.github.com>
2019-10-13 03:01:31 +0200

    BLED -- Home page : Gradient add to few sliders + background
    Signed-off-by: skyultime <32228139+skyultime@users.noreply.github.com>

--- app/src/main/java/fr/bled/ui/home/HomeFragment.java
+++ app/src/main/java/fr/bled/ui/home/HomeFragment.java
@@ -1,16 +1,32 @@
 package fr.bled.ui.home;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import android.widget.ProgressBar;
+import android.graphics.Color;
+import android.graphics.DrawFilter;
+import android.graphics.LinearGradient;
+
+
 
 import fr.bled.R;
 
@@ -18,6 +34,7 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
@@ -30,6 +47,35 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+/*
+        SeekBar seekBarFont0 = root.findViewById(R.id.seekBar);
+        SeekBar seekBarFont1 = root.findViewById(R.id.seekBar2);
+        SeekBar seekBarFont2 = root.findViewById(R.id.seekBar3);
+        SeekBar seekBarFont3 = root.findViewById(R.id.seekBar4);
+
+        SeekBar[] seekBarFontTab = {seekBarFont0,seekBarFont1,seekBarFont2,seekBarFont3};
+
+
+       LinearGradient test = new LinearGradient(0.f, 0.f, 300.f, 0.0f,
+
+                new int[] { 0xFF000000, 0xFF0000FF, 0xFF00FF00, 0xFF00FFFF,
+                        0xFFFF0000, 0xFFFF00FF, 0xFFFFFF00, 0xFFFFFFFF},
+                null, Shader.TileMode.CLAMP);
+        ShapeDrawable shape = new ShapeDrawable(new RectShape());
+        shape.getPaint().setShader(test);
+
+
+
+        for (int i=0;i<seekBarFontTab.length;i++) {
+            seekBarFontTab[i].setScaleY(seekBarFontTab[i].getScaleY()/ 3);
+            seekBarFontTab[i].setProgressDrawable(new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xFFFF0000, 0xFF0000FF}));
+
+
+        }
+        for (int i=0;i<seekBarFontTab.length;i++){
+            seekBarFontTab[i].setProgressDrawable(new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xFFFF0000, 0xFF0000FF}));
+        }
+*/
         return root;
     }
 }
